b5dbfcc0895fbc7cc2f6ef0cdd3df09600ddbf17
NAVNEETSINGH1 <navneet.singh@careerpower.in>
2017-08-07 03:02:31 +0530

    changes for submit all

--- src/main/java/com/example/controller/LoginController.java
+++ src/main/java/com/example/controller/LoginController.java
@@ -2,8 +2,6 @@ package com.example.controller;
 
 import javax.validation.Valid;
 
-import org.json.JSONArray;
-import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
