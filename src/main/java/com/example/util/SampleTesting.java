b6ef992c2d78b3fd7077e05491e448c063f689ba
anurag.verma <anuragvermknn@gmail.com>
2017-08-20 19:35:31 +0530

    util package added

---
+++ src/main/java/com/example/util/SampleTesting.java
@@ -0,0 +1,107 @@
+/**
+ * @author Anurag
+ * @description 
+ */
+package com.example.util;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author Anurag
+ * @description 
+ */
+public class SampleTesting {
+//
+//  public static void main(String[] args) {
+//      Map<Integer, List<? extends Object>> parameterValueLists = new HashMap<Integer, List<? extends Object>>();
+//
+////      List<String> parameter1 = Arrays.asList("value1", "value2");
+////
+////
+////      List<Integer> parameter2 = new ArrayList<Integer>();
+////      parameter2.add(5);
+////      parameter2.add(6);
+////
+////      List<Boolean> parameter3 = new ArrayList();
+////      parameter3.add(true);
+////      parameter3.add(false);
+////      parameter3.add(null);
+////
+////      parameterValueLists.put(new Integer(0), parameter1);
+////      parameterValueLists.put(new Integer(0), parameter2);
+////      parameterValueLists.put(new Integer(0), parameter3);
+////
+////      List<Integer> sizes = new ArrayList();
+////      sizes.add(2);
+////      sizes.add(2);
+////      sizes.add(3);
+////
+////      // char set2[] = {'a', 'b', 'c', 'd'};
+////      int set2[] = {0, 1, 2}; // no of parameters
+////      int k = 3;
+////      System.out.println("hi");
+////      Map<String, List<Integer>> KLengthStrings = AllKLengthStrings.printAllKLength(set2, k, sizes);
+////      for (List<Integer> list : KLengthStrings.values()) {
+////          String parameter1Value = parameter1.get(list.get(0));
+////          Integer parameter2Value = parameter2.get(list.get(1));
+////          Boolean parameter3Value = parameter3.get(list.get(2));
+////          System.out.println(parameter1Value + " " + parameter2Value + " " + parameter3Value);
+////      }
+////
+//      
+//      List<String> parameter1 = Arrays.asList("1A", "1B");
+//      List<String> parameter2 = Arrays.asList("2A", "2B");
+//      List<String> parameter3 = Arrays.asList("3A", "3B");
+//      List<String> parameter4 = Arrays.asList("4A", "4B");
+//      List<String> parameter5 = Arrays.asList("5A", "5B","5C");
+//      List<String> parameter6 = Arrays.asList("6A", "6B");
+//      List<String> parameter7 = Arrays.asList("7A", "7B");
+//
+//
+//
+//
+//      List<Integer> sizes = new ArrayList();
+//      sizes.add(2);
+//      sizes.add(2);
+//      sizes.add(2);
+//      sizes.add(2);
+//      sizes.add(3);
+//      sizes.add(2);
+//      sizes.add(2);
+//
+//      // char set2[] = {'a', 'b', 'c', 'd'};
+//      int set2[] = {0, 1, 2, 3, 4, 5, 6}; // no of parameters
+//      int k = 7;
+//      //System.out.println("hi");
+//      try {
+//      FileWriter fw = new FileWriter("/Users/pallavgarg/Documents/freelancing/combinations");
+//      Map<String, List<Integer>> KLengthStrings = AllKLengthStrings.printAllKLength(set2, k, sizes);
+//      for (List<Integer> list : KLengthStrings.values()) {
+//          String parameter1Value = parameter1.get(list.get(0));
+//          String parameter2Value = parameter2.get(list.get(1));
+//          String parameter3Value = parameter3.get(list.get(2));
+//          String parameter4Value = parameter4.get(list.get(3));
+//          String parameter5Value = parameter5.get(list.get(4));
+//          String parameter6Value = parameter6.get(list.get(5));
+//          String parameter7Value = parameter7.get(list.get(6));
+//          String combination = parameter1Value + "-" + parameter2Value + "-" + parameter3Value+ "-"+parameter4Value + "-" + parameter5Value + "-" + parameter6Value+ "-" + parameter7Value;
+//          System.out.println(combination);
+//          fw.write(combination + "\n");
+//      }
+//      
+//        fw.close();
+//      } catch (IOException e) {
+//        // TODO Auto-generated catch block
+//        e.printStackTrace();
+//      }
+//
+//      
+//  }
+//
+}
