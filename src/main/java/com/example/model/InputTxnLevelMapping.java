c2475faccd1d99cb73412a9c70fa6705e75aafbc
anurag.verma <anuragvermknn@gmail.com>
2017-09-02 01:07:37 -0700

    set soft_delete value in constructor

--- src/main/java/com/example/model/InputTxnLevelMapping.java
+++ src/main/java/com/example/model/InputTxnLevelMapping.java
@@ -38,9 +38,12 @@ public class InputTxnLevelMapping {
   @Column(name = "id")
   private int id;
  
-  @Column(name = "softDelete")
-  private boolean softDelete = false;
+//  @Column(name = "softDelete")
+//  private boolean softDelete = false;
 
+  @Column(name = "softDelete", columnDefinition = "boolean default false", nullable = false)
+  private Boolean softDelete = false;
+  
 //  @AttributeOverrides({
 //    @AttributeOverride(name="identifierID", column = @Column(name="level1Value")),
 //    @AttributeOverride(name="uom", column = @Column(name="level1Name"))
